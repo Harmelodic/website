9e9d9ac28c73c305b09d93d84849aeca80731e9f
Harmelodic <matt@harmelodic.com>
2018-11-10 21:24:07 +0000

    Restructuring into Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Home from "../routes/Home";
+import { Switch, Redirect, Route } from "react-router-dom";
+import Header from "../components/Header";
+import Projects from "../routes/Projects";
 
 export default class App extends React.Component {
     render() {
         return (
-            <Switch>
-                <Route path="/" component={Home} />
-            </Switch>
+            <div>
+                <Header />
+                <Switch>
+                    <Redirect exact from="/" to="/projects" />
+                    <Route path="/projects" component={Projects} />
+                </Switch>
+            </div>
         )
     }
 }
