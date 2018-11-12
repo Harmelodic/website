aa3ce0f241eff372651cf13b1cfbba5033e21f9b
Harmelodic <matt@harmelodic.com>
2018-11-12 20:01:16 +0000

    Styling the Patreon button and added Space

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
+import styled from "styled-components";
 import Header from "../components/Header";
 import Projects from "../routes/Projects";
 import OpenSource from "../routes/OpenSource";
 import Me from "../routes/Me";
+
+const StyledAppBottomSpace = styled.div`
+    width: 100%;
+    height: 50vh;
+`
 
 export default class App extends React.Component {
     render() {
@@ -16,6 +22,7 @@ export default class App extends React.Component {
                     <Route path="/open-source" component={OpenSource} />
                     <Route path="/me" component={Me} />
                 </Switch>
+                <StyledAppBottomSpace />
             </div>
         )
     }
