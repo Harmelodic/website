7595d5106680966928580b0020cc5ec9a8d27f7f
Harmelodic <matt@harmelodic.com>
2019-03-24 14:35:41 +0000

    Removing work twitter because it's bad branding

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -23,7 +23,7 @@ const StyledImage = styled.img`
 export default class SocialMedia extends React.Component {
     render() {
         return (
-            <StyledSocialMedia href={this.props.href} target="_blank">
+            <StyledSocialMedia href={this.props.href} target="_blank" rel="me">
                 <StyledImage src={this.props.src}/>
             </StyledSocialMedia>
         )
