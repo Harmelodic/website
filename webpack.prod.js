c865a3dbfcdf00c28b6674370f83e9d899b68e3c
Harmelodic <matt@harmelodic.com>
2019-04-29 21:05:38 +0100

    Fixed problem with compress and uglify was throwing an error that I don't know how to fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const config = require('./webpack.config.js');
 
@@ -8,9 +7,8 @@ module.exports = merge(config, {
     mode: "production",
     plugins: [
         new MinifyPlugin(),
-        new UglifyJSPlugin(),
         new CompressionPlugin({
-            asset: "[path].gz[query]",
+            filename: "[path].gz[query]",
             algorithm: "gzip",
             test: /\.js$|\.css$|\.html$/,
             threshold: 10240,
