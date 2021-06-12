3a634fae7f8e898f8dd95e3dd10bb43eec6b0dc9
Harmelodic <matt@harmelodic.com>
2021-06-13 00:21:27 +0200

    Bunch of restyling on the posts

--- src/app/main/blog/post-view/PostView.js
+++ src/app/main/blog/post-view/PostView.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { Markdown } from '@harmelodic/react-ui-lib';
 import { Main } from '../../Main';
-import { setBlogTitle, clearSelectedPost, clearMarkdownText } from './actions';
+import { clearSelectedPost, clearMarkdownText } from './actions';
 import { fetchPost, fetchMarkdown } from './middleware';
 
 const StyledViewPost = styled(Main)`
@@ -12,7 +12,7 @@ const StyledViewPost = styled(Main)`
   align-items: flex-start;
   padding-left: 10px;
 
-  /* Markdown re-styling */
+  // Markdown re-styling
   --link-color: #0645ad;
   --link-active-color: #df0000;
   --code-block-background: #000;
@@ -23,10 +23,24 @@ const StyledViewPost = styled(Main)`
   --keyboard-outer-boxshadow: rgba(12, 13, 14, 0.2);
   --keyboard-inner-boxshadow: #fff;
 
+  // Header styling
   & > .heading, h1, h2, h3, h4, h5, h6 {
     font-family: Helvetica, sans-serif;
   }
+  & > div > h1 {
+    padding-bottom: 15px;
+    border-bottom: solid #333 2px;
+  }
+  & > div > h2 {
+    padding-top: 20px;
+    padding-bottom: 10px;
+    border-bottom: solid #888 1px;
+  }
+  & > .heading, h3, h4, h5, h6 {
+    padding-top: 15px;
+  }
 
+  // Creating a gap for clean #anchor linking
   & > div > h1:before, 
   & > div > h2:before, 
   & > div > h3:before {
@@ -65,11 +79,12 @@ export default function PostView(props) {
     };
   }, []);
 
+  const title = selectedPost.title || '';
+
   return (
     <StyledViewPost>
-      <h1>{selectedPost.title}</h1>
       <Markdown
-        markdown={markdownText}
+        markdown={`<h1>${title}</h1>\n\n` + markdownText}
         aTagAttributes='target="_blank" rel="nofollow"'/>
       <StyledCategory>
         {markdownText && selectedPost.category}
