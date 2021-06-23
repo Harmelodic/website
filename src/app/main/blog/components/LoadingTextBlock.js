f21660eb167408ed8d2887987adb51d567f398e8
Harmelodic <matt@harmelodic.com>
2021-06-23 19:08:35 +0200

    Add shimmer effect

--- src/app/main/blog/components/LoadingTextBlock.js
+++ src/app/main/blog/components/LoadingTextBlock.js
@@ -3,7 +3,33 @@ import styled from 'styled-components';
 export const LoadingTextBlock = styled.div`
     height: ${props => props.height ? props.height : '13'}px;
     width: ${props => props.width}px;
+    margin: ${props => props.margin ? props.margin : '5'}px;
     border-radius: ${props => props.height ? props.height : '13'}px;
     background: ${props => props.color ? props.color : '#ccc'};
-    margin: ${props => props.margin ? props.margin : '5'}px;
+    background-image: linear-gradient(
+        to right,
+        ${props => props.color ? props.color : '#ccc'} 0%,
+        rgba(255,255,255,0.5) 20%,
+        ${props => props.color ? props.color : '#ccc'} 40%,
+        ${props => props.color ? props.color : '#ccc'} 60%,
+        ${props => props.color ? props.color : '#ccc'} 80%,
+        ${props => props.color ? props.color : '#ccc'} 100%);
+    background-repeat: no-repeat;
+    background-size: 700 100px; 
+    
+    animation-duration: 1300ms;
+    animation-fill-mode: forwards; 
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    animation-name: shimmer;
+
+    @keyframes shimmer {
+        0% {
+            background-position: calc(-${props => props.width}px * 5) 0;
+        }
+        
+        100% {
+            background-position: calc(${props => props.width}px * 5) 0;
+        }
+    }
 `;
