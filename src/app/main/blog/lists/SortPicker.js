211f49df7838037ecbf2d9100545057b25b315f0
Harmelodic <matt@harmelodic.com>
2022-01-04 21:49:53 +0100

    Flexing SortPicker

--- src/app/main/blog/lists/SortPicker.js
+++ src/app/main/blog/lists/SortPicker.js
@@ -1,41 +1,29 @@
 import styled from 'styled-components';
 
-const pickerHeight = 80;
 const StyledSortPicker = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+    align-items: center;
 	width: 100%;
-	height: ${pickerHeight}px;
+	height: 80px;
 	border-bottom: solid 1px #bbb;
 `;
 
-const StyledSortChoiceWrapper = styled.div`
-	display: inline-block;
-	width: calc(100%/3);
-	height: ${pickerHeight};
-	text-align: center;
-`;
-
-const logoSize = 30;
-const StyledSortLogo = styled.img`
-	display: inline-block;
-	margin: ${(pickerHeight - logoSize) / 2}px;
-	width: ${logoSize}px;
-	height: ${logoSize}px;
+const SortLogo = styled.img`
+	display: flex;
+    margin-left: 25px;
+	width: 30px;
+	height: 30px;
 	background: ${props => props.isSelected ? '#333' : '#bbb'};
 	transition: background 150ms;
 `;
 
-const StyledSortText = styled.div`
-	display: inline-block;
-	margin-right: 23px;
-	height: ${pickerHeight}px;
-	font-size: 18px;
-	line-height: 80px;
-	vertical-align: top;
-`;
-
-const StyledSortChoice = styled.div`
-	display: inline-block;
-	height: ${pickerHeight}px;
+const SortChoice = styled.div`
+	display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+	height: 100%;
 	cursor: pointer;
 	color: ${props => props.isSelected ? '#333' : '#bbb'};
 	transition: color 150ms;
@@ -44,10 +32,20 @@ const StyledSortChoice = styled.div`
 		color: #000;
 	}
 
-	&:hover ${StyledSortLogo} {
+	&:hover ${SortLogo} {
 		background: #333;
 	}
 `;
+
+const SortText = styled.div`
+	display: flex;
+	margin-left: 25px;
+	height: 100%;
+	font-size: 18px;
+	line-height: 80px;
+	vertical-align: top;
+`;
+
 
 export default function SortPicker(props) {
 	const sortChoices = [
@@ -73,20 +71,19 @@ export default function SortPicker(props) {
 			{
 				sortChoices.map((choice) => {
 					return (
-						<StyledSortChoiceWrapper key={choice.sort}>
-							<StyledSortChoice
-								onClick={() => {
-									props.onChangeSort(choice.sort);
-								}}
+						<SortChoice
+							key={choice.sort}
+							onClick={() => {
+								props.onChangeSort(choice.sort);
+							}}
+							isSelected={(choice.sort === props.selectedChoice)}
+						>
+							<SortLogo
+								src={choice.img}
 								isSelected={(choice.sort === props.selectedChoice)}
-							>
-								<StyledSortLogo
-									src={choice.img}
-									isSelected={(choice.sort === props.selectedChoice)}
-								/>
-								<StyledSortText>{choice.display}</StyledSortText>
-							</StyledSortChoice>
-						</StyledSortChoiceWrapper>
+							/>
+							<SortText>{choice.display}</SortText>
+						</SortChoice>
 					);
 				})
 			}
