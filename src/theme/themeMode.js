05de864c325acb1d9393896658a23364741b20da
Harmelodic <matt@harmelodic.com>
2023-08-02 00:05:59 +0200

    theme mode in localstorage for saving between refreshes

--- src/theme/themeMode.js
+++ src/theme/themeMode.js
@@ -3,17 +3,20 @@ import { createSlice } from '@reduxjs/toolkit';
 export const themeMode = createSlice({
 	name: 'themeMode',
 	initialState: {
-		value: 'system-preference',
+		value: localStorage.getItem('themeMode') || 'system-preference',
 	},
 	reducers: {
 		setToSystemPreference: (state) => {
 			state.value = 'system-preference';
+			localStorage.setItem('themeMode', 'system-preference');
 		},
 		setToLight: (state) => {
 			state.value = 'light';
+			localStorage.setItem('themeMode', 'light');
 		},
 		setToDark: (state) => {
 			state.value = 'dark';
+			localStorage.setItem('themeMode', 'dark');
 		},
 	},
 });
