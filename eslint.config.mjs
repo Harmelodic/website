dfa8eafc9652e1df50a9399259be2e1e8965e16f
Harmelodic <matt@harmelodic.com>
2024-04-09 21:18:02 +0200

    Migrate to new eslint structure, eslint-google is archived, fixing most faults

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import react from 'eslint-plugin-react';
+import jsdoc from 'eslint-plugin-jsdoc';
+
+export default [
+    {
+        name: "custom",
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                browsers: true
+            },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                },
+            },
+        },
+        plugins: {
+            react: react,
+            jsdoc: jsdoc
+        },
+        rules: {
+            "arrow-parens": ["error", "as-needed", {"requireForBlockBody": false}],
+            "indent": ["error", "tab", {"SwitchCase": 1}],
+            "max-len": ["error", {"code": 120}],
+            "object-curly-spacing": ["error", "always"],
+            "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+            "no-tabs": ["error", {"allowIndentationTabs": true}],
+            // "react/jsx-uses-vars": ["error"], // broken: "TypeError: context.markVariableAsUsed is not a function"
+            "jsdoc/require-jsdoc": ["error", {
+                "require": {
+                    "FunctionDeclaration": false,
+                    "MethodDefinition": false,
+                    "ClassDeclaration": false,
+                    "ArrowFunctionExpression": false,
+                    "FunctionExpression": false,
+                }
+            }]
+        }
+    }
+]
