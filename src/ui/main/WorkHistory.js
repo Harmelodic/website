6663f1d240c14b6839c656ba741756e4289f6ef4
Harmelodic <matt@harmelodic.com>
2024-06-09 16:00:42 +0200

    Made work history look more like a timeline

--- src/ui/main/WorkHistory.js
+++ src/ui/main/WorkHistory.js
@@ -10,11 +10,20 @@ const WorkTitle = styled.span`
     font-weight: ${props => props.theme.font.weight.bold};
 `;
 
+const VerticalLine = styled.div`
+	width: 0;
+	height: 50px;
+	border-left: solid 1px ${props => props.theme.colors.softBorder};
+	margin: 10px 0;
+`;
+
 export default function WorkHistory() {
 	return (
 		<Main>
 			<Title>Work History</Title>
+			
 			<Separator />
+			
 			<ColumnInfoBox>
 				I believe in Pay Transparency, which is why below I have listed the highest salary I achieved for
 				each role.
@@ -22,26 +31,22 @@ export default function WorkHistory() {
 				If you believe you are being underpaid for your work, then please raise this with your employer or
 				union.
 			</ColumnInfoBox>
+			
 			<Separator />
+			
 			<ColumnInfoBox>
 				<WorkTitle>Software Engineer @ Nordnet</WorkTitle>
 				<span>May 2021 - Current</span>
 				<span>76,500 kr / month</span>
-			</ColumnInfoBox>
-			<Separator />
-			<ColumnInfoBox>
+				<VerticalLine />
 				<WorkTitle>Director @ Coding for Immigrants (CFI)</WorkTitle>
 				<span>September 2022 - May 2024</span>
 				<span>Volunteer</span>
-			</ColumnInfoBox>
-			<Separator />
-			<ColumnInfoBox>
+				<VerticalLine />
 				<WorkTitle>Engineering Manager @ Klarna</WorkTitle>
 				<span>August 2020 - May 2021</span>
 				<span>68,967 kr / month</span>
-			</ColumnInfoBox>
-			<Separator />
-			<ColumnInfoBox>
+				<VerticalLine />
 				<WorkTitle>Software Engineer Lead @ Capgemini</WorkTitle>
 				<span>September 2014 - August 2020</span>
 				<span>£49,425 / year</span>
